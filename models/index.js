a02a7b213a5671612fdede178f98372a3fd518d4
lordshadowist <shadowist@live.com>
2016-09-25 17:43:38 -0700

    ID NAN debug

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
+//mongoose.Promise = global.Promise;
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
 
 // module.exports.Campsite = require("./campsite.js.example");
